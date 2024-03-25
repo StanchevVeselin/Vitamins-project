959789bfdcc0bb71b5d5f1d074c0d556165d326b
Stanchevvv <122915555+Stanchevvv@users.noreply.github.com>
2024-03-25 20:44:32 +0200

    main components
    main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './navigation/nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    ProductItemComponent,
+    CatalogComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
