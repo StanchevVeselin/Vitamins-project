0a22e4fa7bcfb124bfbb9725554d519187a6a3b7
Stanchevvv <122915555+Stanchevvv@users.noreply.github.com>
2024-03-28 13:46:15 +0200

    Update comments.ts

--- src/app/types/comments.ts
+++ src/app/types/comments.ts
@@ -1,6 +1,23 @@
 export interface Comments {
     email: string,
-    content: string,
-    _ownerId: string,
+    text: string,
+    _createdOn: string,
     _id: string,
+    __ownerId:string
 }
+
+// email
+// : 
+// "324"
+// text
+// : 
+// "1234"
+// _createdOn
+// : 
+// 1711626056183
+// _id
+// : 
+// "384decd0-3fee-4950-9d6b-5df9131e0825"
+// _ownerId
+// : 
+// "6f3722ec-788e-486d-8e39-e4a0a24d2690"
