72ec2aaf2c99fc748502783f60ec7988ebd07bf8
Stanchevvv <122915555+Stanchevvv@users.noreply.github.com>
2024-04-02 11:55:49 +0300

    Login formating

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../types/user';
 import { environment } from 'src/environments/environment.development';
-import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +14,19 @@ export class UserService {
     const {apiUrlRegister} = environment
     
       this.http.post<User>(apiUrlRegister, {email,password}).subscribe((response) => {
+        console.log(response);
+        this.login(email,password)
+        
     }
     )
   }
 
 
-  login(email:string, password:string) {
+  login(email:string, password:string){
     const {apiUrlLogin} = environment
 
-    this.http.post<User>(apiUrlLogin, {email,password}).subscribe((response) => {
+    this.http.post<User>(apiUrlLogin, {email,password})
+    .subscribe((response) => {
       console.log(response);
       sessionStorage.setItem('email', response.email);
       sessionStorage.setItem("accessToken",response.accessToken)
